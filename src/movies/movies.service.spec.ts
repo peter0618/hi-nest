4f5cce2cd6be4d53cc771c521a8c33f5d51ddf0e
peter0618 <peterchoi0618@gmail.com>
2021-02-02 01:59:07 +0900

    delete, create, update 에 대한 test code 작성

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -25,7 +25,7 @@ describe('MoviesService', () => {
   });
 
   describe('getOne', () => {
-    it('should return a movie', () =>{
+    it('should return a movie', () => {
       service.create({
         title: 'Test Movie',
         genres: ['test'],
@@ -38,13 +38,68 @@ describe('MoviesService', () => {
       expect(movie.title).toEqual('Test Movie');
       expect(movie.year).toEqual(2000);
     });
-    it('should throw 404 error', ()=>{
-      try{
+    it('should throw 404 error', () => {
+      try {
         service.getOne(999);
-      } catch (e){
+      } catch (e) {
         expect(e).toBeInstanceOf(NotFoundException);
         expect(e.message.message).toEqual('Movie with ID: 999 not found.');
       }
     });
   });
+
+  describe('deleteOne', () => {
+    it('deletes a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+    it('should return a 404', () =>{
+      try {
+        service.deleteOne(999)
+      } catch (e){
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    })
+  });
+
+  describe('create', () => {
+    it('should create a movie', () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const afterCreate = service.getAll().length;
+      console.log(beforeCreate, afterCreate);
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    })
+  })
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      service.update(1, {title:'Updated Test'});
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Updated Test');
+    });
+    it('should throw a NotFoundException', () =>{
+      try {
+        service.update(999,{})
+      } catch (e){
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    })
+  })
 });
